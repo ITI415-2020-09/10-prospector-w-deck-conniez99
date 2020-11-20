7bced63f2370a6ebd656d15c999e478a59c60ca3
conniez99 <49135897+conniez99@users.noreply.github.com>
2020-11-20 15:09:57 -0500

    score script

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -16,8 +16,10 @@ public class Prospector : MonoBehaviour
 	public float xOffset = 3;
 	public float yOffset = -2.5f;
 	public Vector3 layoutCenter;
-
-
+	public Vector2 fsPosMid = new Vector2(0.5f, 0.90f);
+	public Vector2 fsPosRun = new Vector2(0.5f, 0.75f);
+	public Vector2 fsPosMid2 = new Vector2(0.4f, 1.0f);
+	public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
 
 
 	[Header("Set Dynamically")]
@@ -28,7 +30,7 @@ public class Prospector : MonoBehaviour
 	public CardProspector target;
 	public List<CardProspector> tableau;
 	public List<CardProspector> discardPile;
-
+	public FloatingScore fsRun;
 
 	void Awake()
 	{
@@ -37,6 +39,8 @@ public class Prospector : MonoBehaviour
 
 	void Start()
 	{
+		Scoreboard.S.score = ScoreManager.SCORE;
+
 		deck = GetComponent<Deck>();
 		deck.InitDeck(deckXML.text);
 		Deck.Shuffle(ref deck.cards);
@@ -151,8 +155,8 @@ public class Prospector : MonoBehaviour
 		}
 	}
 
-		// Moves the current target to the discardPile
-		void MoveToDiscard(CardProspector cd)
+	// Moves the current target to the discardPile
+	void MoveToDiscard(CardProspector cd)
 	{
 		// Set the state of the card to discard
 		cd.state = eCardState.discard;
@@ -220,6 +224,7 @@ public class Prospector : MonoBehaviour
 				MoveToTarget(Draw());
 				UpdateDrawPile();
 				ScoreManager.EVENT(eScoreEvent.draw);
+				FloatingScoreHandler(eScoreEvent.draw);
 				break;
 			case eCardState.tableau:
 				bool validMatch = true;
@@ -239,6 +244,7 @@ public class Prospector : MonoBehaviour
 				MoveToTarget(cd); // Make it the target card
 				SetTableauFaces(); // Update tableau card face-ups
 				ScoreManager.EVENT(eScoreEvent.mine);
+				FloatingScoreHandler(eScoreEvent.mine);
 				break;
 		}
 		// Check to see whether the game is over or not
@@ -281,11 +287,13 @@ public class Prospector : MonoBehaviour
 		{
 			//print("Game Over. You won! :)");
 			ScoreManager.EVENT(eScoreEvent.gameWin);
+			FloatingScoreHandler(eScoreEvent.gameWin);
 		}
 		else
 		{
 			//print("Game Over. You Lost. :(");
 			ScoreManager.EVENT(eScoreEvent.gameLoss);
+			FloatingScoreHandler(eScoreEvent.gameLoss);
 		}
 		// Reload the scene, resetting the game
 		SceneManager.LoadScene("__Prospector_Scene_0");
@@ -306,5 +314,54 @@ public class Prospector : MonoBehaviour
 		if (c0.rank == 13 && c1.rank == 1) return (true);
 		// Otherwise, return false
 		return (false);
+	}
+
+	void FloatingScoreHandler(eScoreEvent evt)
+	{
+		List<Vector2> fsPts;
+		switch (evt)
+		{
+			case eScoreEvent.draw: // Drawing a card
+			case eScoreEvent.gameWin: // Won the round
+			case eScoreEvent.gameLoss: // Lost the round
+									   // Add fsRun to the Scoreboard score
+				if (fsRun != null)
+				{
+					// Create points for the Bézier curve1
+					fsPts = new List<Vector2>();
+					fsPts.Add(fsPosRun);
+					fsPts.Add(fsPosMid2);
+					fsPts.Add(fsPosEnd);
+					fsRun.reportFinishTo = Scoreboard.S.gameObject;
+					fsRun.Init(fsPts, 0, 1);
+					// Also adjust the fontSize
+					fsRun.fontSizes = new List<float>(new float[] { 28, 36, 4 });
+					fsRun = null;
+				}
+				break;
+			case eScoreEvent.mine: // Remove a mine card
+								   // Create a FloatingScore for this score
+				FloatingScore fs;
+				// Move it from the mousePosition to fsPosRun
+				Vector2 p0 = Input.mousePosition;
+				p0.x /= Screen.width;
+				p0.y /= Screen.height;
+				fsPts = new List<Vector2>();
+				fsPts.Add(p0);
+				fsPts.Add(fsPosMid);
+				fsPts.Add(fsPosRun);
+				fs = Scoreboard.S.CreateFloatingScore(ScoreManager.CHAIN, fsPts);
+				fs.fontSizes = new List<float>(new float[] { 4, 50, 28 });
+				if (fsRun == null)
+				{
+					fsRun = fs;
+					fsRun.reportFinishTo = null;
+				}
+				else
+				{
+					fs.reportFinishTo = fsRun.gameObject;
+				}
+				break;
+		}
 	}
 }
